53e21c3eb25e49daf5257995c31ab8ddbe7faf79
Abdel Ghani Muhra <67690813+AbdelMuhra@users.noreply.github.com>
2021-12-24 06:42:05 +0000

    used just one int/float data type conversion in the input of each variable instead of multiple times in the calculation.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 print("Welcome to the tip calculator!")
-total = input("What was the total bill? $")
-tip = input("How much tip would you like to give? 10,12 or 15? ")
-people = input("how many people are going to split the bill? ")
-Total_per_Person =float(total)/int(people) + (float(total)*(float(tip)/100))/int(people)
+total = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10,12 or 15? "))
+people = int(input("how many people are going to split the bill? "))
+Total_per_Person =total/people +total*(tip/100)/people
 final_amount=round(Total_per_Person,2)
 print(f"Each person should pay: ${final_amount}")
 
