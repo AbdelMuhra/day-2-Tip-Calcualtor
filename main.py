c8b64a8237b63bbfd1e99620e043ea5dd1736b8c
Abdel Ghani Muhra <67690813+AbdelMuhra@users.noreply.github.com>
2021-12-24 06:36:44 +0000

    fixed a data type error.

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ print("Welcome to the tip calculator!")
 total = input("What was the total bill? $")
 tip = input("How much tip would you like to give? 10,12 or 15? ")
 people = input("how many people are going to split the bill? ")
-Total_per_Person =int(total)/int(people) + (int(total)*(int(tip)/100))/int(people)
-final_amount=round(Total_per_Person)
+Total_per_Person =float(total)/int(people) + (float(total)*(float(tip)/100))/int(people)
+final_amount=round(Total_per_Person,2)
 print(f"Each person should pay: ${final_amount}")
 
 
