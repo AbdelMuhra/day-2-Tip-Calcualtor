59f1d02c83c1cdc20842fc04bd4bc27045a9689d
Abdel Ghani Muhra <67690813+AbdelMuhra@users.noreply.github.com>
2021-12-24 06:32:57 +0000

    used the F-string instead of Str() funtion and the round function to eliminate decimals

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ total = input("What was the total bill? $")
 tip = input("How much tip would you like to give? 10,12 or 15? ")
 people = input("how many people are going to split the bill? ")
 Total_per_Person =int(total)/int(people) + (int(total)*(int(tip)/100))/int(people)
-final_amount=round(Total_per_Person,2)
-print("Each person should pay: $"+str(final_amount))
+final_amount=round(Total_per_Person)
+print(f"Each person should pay: ${final_amount}")
+
+
